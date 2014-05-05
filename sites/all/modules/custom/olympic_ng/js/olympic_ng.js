d5b6edb866f28a77186b950c8b66e28de9fa4499
Frank Baele <frank.baele@xio.be>
2014-05-05 21:43:54 +0200

    add autocomplete and sport and country search

--- sites/all/modules/custom/olympic_ng/js/olympic_ng.js
+++ sites/all/modules/custom/olympic_ng/js/olympic_ng.js
@@ -25,19 +25,26 @@ olympic_ng.app.controller('page', function($scope, search, $location) {
   $scope.search = {
     'keyword': '',
     'year': '',
+    'sport': '',
+    'country': '',
     'page': 1
   }
-
+  $scope.countries = olympic_ng.countries;
+  $scope.sports = olympic_ng.sports;
   _.extend($scope.search, $location.search());
 
+
   $scope.searchInput = $scope.search.keyword;
+  $scope.sport = _.findWhere($scope.sports, {tid: $scope.search.sport});
+  $scope.country = _.findWhere($scope.countries, {tid: $scope.search.country});
   $scope.launchSearch = function (newSearch) {
     if (newSearch){
       $scope.search.keyword = $scope.searchInput;
       $scope.search.page = 1;
       var promise = search.getResults({
         'search' : $scope.search.keyword,
-        'year' : $scope.search.year,
+        'country' : $scope.search.country,
+        'sport' : $scope.search.sport,
         'page' : 0,
         'per_page' : $scope.results_per_page
       });
@@ -45,6 +52,8 @@ olympic_ng.app.controller('page', function($scope, search, $location) {
       var promise = search.getResults({
         'search' : $scope.search.keyword,
         'year' : $scope.search.year,
+        'country' : $scope.search.country,
+        'sport' : $scope.search.sport,
         'page' : parseInt($scope.search.page) - 1,
         'per_page' : $scope.results_per_page
       });
@@ -60,9 +69,12 @@ olympic_ng.app.controller('page', function($scope, search, $location) {
 
   $scope.createUrlWithCurrentState = function () {
     $location.$search = {};
+    console.log($scope.search);
     _.each($scope.search, function (val, key) {
       if (val) {
         $location.search(key, val);
+      } else {
+        $location.search(key, null);
       }
     })
   }
@@ -72,12 +84,30 @@ olympic_ng.app.controller('page', function($scope, search, $location) {
       $scope.launchSearch(false);
       $scope.createUrlWithCurrentState();
     }
-  })
-  $scope.$watchCollection('search', function(newValue, oldValue){
+  });
+
+  $scope.$watch('sport', function(newValue, oldValue){
     if(newValue != oldValue){
+      $scope.search.sport = newValue.tid;
       $scope.createUrlWithCurrentState();
+      $scope.launchSearch(true);
     }
-  })
+  }, true);
+
+  $scope.$watch('country', function(newValue, oldValue){
+    if(newValue != oldValue){
+      $scope.search.country = newValue.tid;
+      $scope.createUrlWithCurrentState();
+      $scope.launchSearch(true);
+    }
+  }, true);
+  $scope.$watch('searchInput', function(newValue, oldValue){
+    if(newValue != oldValue){
+      $scope.search.keyword = newValue;
+      $scope.createUrlWithCurrentState();
+      $scope.launchSearch(true);
+    }
+  }, true);
   $scope.launchSearch(false);
 
 });
