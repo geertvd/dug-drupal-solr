4f9fdfbdea0520ea17c0a71eb4690baf870e82f1
Frank Baele <frank.baele@xio.be>
2014-05-07 16:49:42 +0200

    add taxonomy filtering

--- sites/all/modules/custom/olympic_ng/js/olympic_ng.js
+++ sites/all/modules/custom/olympic_ng/js/olympic_ng.js
@@ -19,10 +19,13 @@ olympic_ng.app.controller('page', function($scope, search, $location) {
   $scope.results = {};
   $scope.total_results = 0;
 
-  $scope.launchSearchEasySearch = function(){
+  $scope.launchSearch = function () {
     var promise = search.getResults({
-      search : $scope.searchInput
+      'search' : $scope.searchInput,
+      'country' : $scope.country.tid,
+      'sport' : $scope.sport.tid
     });
+
     promise.then(function (result) {
       $scope.total_results = result.total_results;
       /** When the list is returned add to our view */
@@ -30,7 +33,16 @@ olympic_ng.app.controller('page', function($scope, search, $location) {
     });
   }
 
-  //$scope.launchSearchEasySearch();
+  /* Taxonomy filters */
+
+  $scope.countries = olympic_ng.countries;
+  $scope.sports = olympic_ng.sports;
+
+  /* Search input */
+  $scope.searchInput = '';
+  $scope.sport = '';
+  $scope.country = '';
+
 });
 /*
  * angular-ui-bootstrap
--- sites/all/modules/custom/olympic_ng/templates/drupal/olympic_ng_search_page.tpl.php
+++ sites/all/modules/custom/olympic_ng/templates/drupal/olympic_ng_search_page.tpl.php
@@ -1,9 +1,17 @@
 <div id="olympicApp">
   <div ng-controller="page">
-    <form role="form" ng-submit="launchSearchEasySearch()">
+    <form role="form" ng-submit="launchSearch()">
       <div class="form-group">
         <label>Free text</label>
         <input class="form-control" ng-model="searchInput">
+      </div>
+      <div class="form-group">
+        <label>Sport</label>
+        <input class="form-control" ng-model="sport"  value="sport.title" typeahead="sport as sport.title for sport in sports | filter:$viewValue | limitTo:8">
+      </div>
+      <div class="form-group">
+        <label>Country</label>
+        <input class="form-control" ng-model="country" value="country.title" typeahead="country as country.title for country in countries | filter:$viewValue | limitTo:8">
       </div>
       <button class="submit btn btn-default" type="submit"><?php print(t('Launch the search')); ?></button>
     </form>
