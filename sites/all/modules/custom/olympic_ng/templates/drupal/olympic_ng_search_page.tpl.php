0c312e9d6ab64fb512b8cc28d150cd6141d25713
Frank Baele <frank.baele@xio.be>
2014-05-07 17:04:34 +0200

    make it shiny

--- sites/all/modules/custom/olympic_ng/templates/drupal/olympic_ng_search_page.tpl.php
+++ sites/all/modules/custom/olympic_ng/templates/drupal/olympic_ng_search_page.tpl.php
@@ -1,6 +1,6 @@
 <div id="olympicApp">
   <div ng-controller="page">
-    <form role="form" ng-submit="launchSearch()">
+    <form role="form" ng-submit="launchSearch(true)">
       <div class="form-group">
         <label>Free text</label>
         <input class="form-control" ng-model="searchInput">
@@ -15,9 +15,36 @@
       </div>
       <button class="submit btn btn-default" type="submit"><?php print(t('Launch the search')); ?></button>
     </form>
-    <div ng-repeat="result in results" >
-      {{result.title}}
-    </div>
+    <table class="table table-striped">
+      <thead>
+      <tr>
+        <th>#</th>
+        <th>Name</th>
+        <th>Age</th>
+        <th>Country</th>
+        <th>Sport</th>
+        <th>Bronze medals</th>
+        <th>Silver medals</th>
+        <th>Gold medals</th>
+        <th>Total medals</th>
+        <th>Year</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr ng-repeat="result in results" >
+        <td>1</td>
+        <td><a target="_self" ng-href="{{result.url}}">{{result.title}}</a></td>
+        <td>{{result.age}}</td>
+        <td>{{result.country}}</td>
+        <td>{{result.sport}}</td>
+        <td>{{result.bronze_medals}}</td>
+        <td>{{result.silver_medals}}</td>
+        <td>{{result.gold_medals}}</td>
+        <td>{{result.total_medals}}</td>
+        <td>{{result.year}}</td>
+      </tr>
+      </tbody>
+    </table>
     <div class="pagination-centered" ng-show="total_results >= results_per_page">
       <pagination total-items="total_results" ng-model="search.page" items-per-page="results_per_page"></pagination>
     </div>
